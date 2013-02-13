9e067ce102deccbdaa8daf5a25c45faae247a75e
Brendan Loyot <b.loyot@umiami.edu>
2013-02-13 14:16:21 -0500

    working server

--- index.js
+++ index.js
@@ -9,17 +9,3 @@ var apikey = process.env.apikey || '';
 var port = process.env.PORT || 8888;
 
 server.start(apikey, port);
-
-/*
-fs.readFile('apikey.txt', 'ascii', function(err, data) {
-   
-    if (err) {
-        return console.log(err);
-    }
- 
-    // slice off the return line char and pass in the key
-    var key = data.slice(0, data.length-1);
-    server.start(key);
-});
-*/
-
